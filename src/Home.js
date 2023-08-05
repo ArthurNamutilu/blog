11f6a3ea4ec6db5ffb2e27713d04c5b05df3c5e1
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-08-05 10:49:18 +0300

    passing argument to function using anonymus function

--- src/Home.js
+++ src/Home.js
@@ -2,10 +2,14 @@ const Home = () => {
     const handleClick = () => {
         console.log('Hi Naomie');
     }
+    const handleClickAgain = (name) => {
+        console.log(`Hi again ${name}`);
+    }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
-            <button onClick={handleClick()}>Click Here</button>
+            <button onClick={handleClick}>Click Here</button>
+            <button onClick={ () => handleClickAgain('Arthur') }>Click again</button>
         </div>
      );
 }
