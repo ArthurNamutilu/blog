f2d7bbde890c398b2fd10b09c9276f0cb5968177
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-08-05 07:10:26 +0300

    dynamic values

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 
 function App() {
+  const title = 'Welcome to this blog';
+  const likes = 58;
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
+        <p>{[1,2,3,4,5]}</p>
+        <p>{Math.random() * 10}</p>
       </div>
     </div>
   );
