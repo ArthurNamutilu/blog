b6a1958f418bc67781609104c12d3c667059da86
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-08-05 10:32:13 +0300

    button event with function

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,11 @@
 const Home = () => {
+    const handleClick = () => {
+        console.log('Hi Naomie');
+    }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick()}>Click Here</button>
         </div>
      );
 }
