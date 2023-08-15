a3f22c0bcb516d4a8fea6b2b3a5a0e42fa69a4f9
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-08-15 20:17:09 +0300

    reusing components

--- src/Home.js
+++ src/Home.js
@@ -5,12 +5,15 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         {title: 'The River and The Source', body: 'Lorem ipsum...', author: 'Magret Ogolla', id: 1},
         {title: 'The Caucasian Chalk Circle', body: 'Lorem ipsum...', author: 'Bertolt Brecht', id: 2},
-        {title: 'The Pearl', body: 'Lorem ipsum...', author: 'John Steinbeck', id: 3}
+        {title: 'The Grapes of Wrath', body: 'Lorem ipsum...', author: 'John Steinbeck', id: 3},
+        {title: 'The Pearl', body: 'Lorem ipsum...', author: 'John Steinbeck', id: 4}
     ]);
-const title = "All Blogs";
+const title = "All blogs";
+const johns = "John Steinbeck blogs";
     return (
         <div className="home">
             < BlogList shared = {blogs} title = { title }/>
+            < BlogList shared = {blogs.filter((blog) => blog.author === 'John Steinbeck')} title = { johns }/>
         </div>
     );
 }
